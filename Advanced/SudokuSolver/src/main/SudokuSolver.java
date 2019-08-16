5cc955383f343ed26b9db46e434b004bd36e000c
AvDalfsen <AvDalfsen@sogyo.nl>
2019-08-16 08:34:44 +0200

    Sudoku Solver start

---
+++ Advanced/SudokuSolver/src/main/SudokuSolver.java
@@ -0,0 +1,14 @@
+package main;
+
+public class SudokuSolver {
+
+	public SudokuSolver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
